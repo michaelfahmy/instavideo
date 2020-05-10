8849a33884a091589e29de3dd3014a703134c1f5
Michael Fahmy <michaelfahmy1@gmail.com>
2020-05-10 16:46:02 +0200

    Bootstrap installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'stylesheets/application'
+
+import 'bootstrap'
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+})
